586b04fb4f7225323248f677c1347ccc9cc3c37c
王 玉才 <netowls-studio@outlook.com>
2022-08-02 14:25:47 +0800

    feat: 新增默认值类型。

---
+++ src/Eips.Common/Common/Defaults.cs
@@ -0,0 +1,19 @@
+﻿/* *************************************************************************************************************************************** *\
+ * COPYRIGHT © 2006 - 2022 WANG YUCAI. ALL RIGHTS RESERVED.                                                                                *
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                                      *
+\* *************************************************************************************************************************************** */
+
+namespace Niacomsoft.Eips
+{
+    /// <summary> 提供了企业私有 SaaS 服务相关的默认值。 </summary>
+    public class Defaults
+    {
+        /// <summary> 用于初始化一个 <see cref="Defaults" /> 类型的对象实例。 </summary>
+        private Defaults()
+        {
+        }
+
+        /// <summary> 默认的诊断器类别。 </summary>
+        public const string DefaultDiagnosticCategory = "EIPS";
+    }
+}
