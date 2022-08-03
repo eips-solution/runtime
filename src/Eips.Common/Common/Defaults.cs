2eb0214a02be88c1deb6a5e982d8de613e899b31
王 玉才 <netowls-studio@outlook.com>
2022-08-03 13:31:08 +0800

    feat: 新增字符串编码工具类 StringUtilities.Encoding.cs。

--- src/Eips.Common/Common/Defaults.cs
+++ src/Eips.Common/Common/Defaults.cs
@@ -3,6 +3,8 @@
  * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                                      *
 \* *************************************************************************************************************************************** */
 
+using System.Text;
+
 namespace Niacomsoft.Eips
 {
     /// <summary> 提供了企业私有 SaaS 服务相关的默认值。 </summary>
@@ -15,5 +17,13 @@ namespace Niacomsoft.Eips
 
         /// <summary> 默认的诊断器类别。 </summary>
         public const string DefaultDiagnosticCategory = "EIPS";
+
+        /// <summary>
+        /// 默认的编码程序。
+        /// <para> <see cref="Encoding" /> 类型的对象实例。 </para>
+        /// </summary>
+        /// <seealso cref="Encoding" />
+        /// <seealso cref="UTF8Encoding" />
+        public static readonly Encoding DefaultEncoding = Encoding.UTF8;
     }
 }
---
+++ src/Eips.Common/Utilities/StringUtilities.cs
@@ -0,0 +1,16 @@
+﻿/* *************************************************************************************************************************************** *\
+ * COPYRIGHT © 2006 - 2022 WANG YUCAI. ALL RIGHTS RESERVED.                                                                                *
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                                      *
+\* *************************************************************************************************************************************** */
+
+namespace Niacomsoft.Eips
+{
+    /// <summary> 提供了字符串相关的辅助方法。 </summary>
+    public partial class StringUtilities
+    {
+        /// <summary> 用于初始化一个 <see cref="StringUtilities" /> 类型的对象实例。 </summary>
+        private StringUtilities()
+        {
+        }
+    }
+}
