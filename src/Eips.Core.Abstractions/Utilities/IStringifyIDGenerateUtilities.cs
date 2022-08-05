85a9d89308c5f2445449f4912892181e41137981
王 玉才 <netowls-studio@outlook.com>
2022-08-05 16:31:40 +0800

    feat: 新增扩展接口 IStringifyIDGenerateUtilities 等。

---
+++ src/Eips.Core.Abstractions/Utilities/IStringifyIDGenerateUtilities.cs
@@ -0,0 +1,13 @@
+﻿/* *************************************************************************************************************************************** *\
+ * COPYRIGHT © 2006 - 2022 WANG YUCAI. ALL RIGHTS RESERVED.                                                                                *
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                                      *
+\* *************************************************************************************************************************************** */
+
+namespace Niacomsoft.Eips
+{
+    /// <summary> 定义了生成字符串类型 ID 的方法接口。 </summary>
+    /// <seealso cref="IIDGenerateUtilities{T}" />
+    public interface IStringifyIDGenerateUtilities : IIDGenerateUtilities<string>
+    {
+    }
+}
