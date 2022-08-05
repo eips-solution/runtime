3b5d50ab01f231eafd06a19c39b010420ecd63d1
王 玉才 <netowls-studio@outlook.com>
2022-08-05 14:15:48 +0800

    feat: 新增配置选项标记接口 IConfigurableOptions。

---
+++ src/Eips.Core.Abstractions/Core/IConfigurableOptions.cs
@@ -0,0 +1,12 @@
+﻿/* *************************************************************************************************************************************** *\
+ * COPYRIGHT © 2006 - 2022 WANG YUCAI. ALL RIGHTS RESERVED.                                                                                *
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                                      *
+\* *************************************************************************************************************************************** */
+
+namespace Niacomsoft.Eips.Options
+{
+    /// <summary> 定义了可配置选项的标记接口。 </summary>
+    public interface IConfigurableOptions
+    {
+    }
+}
