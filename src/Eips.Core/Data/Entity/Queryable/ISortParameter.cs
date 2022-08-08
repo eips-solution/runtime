1cc8afe979df24256fdb43aa17bcf6bb0f91ab14
王 玉才 <netowls-studio@outlook.com>
2022-08-08 11:29:22 +0800

    feat: 新增数据排序相关的参数实体 ISortParameter。

---
+++ src/Eips.Core/Data/Entity/Queryable/ISortParameter.cs
@@ -0,0 +1,20 @@
+﻿/* *************************************************************************************************************************************** *\
+ * COPYRIGHT © 2006 - 2022 WANG YUCAI. ALL RIGHTS RESERVED.                                                                                *
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                                      *
+\* *************************************************************************************************************************************** */
+
+namespace Niacomsoft.Eips.Data.Entity.Queryable
+{
+    /// <summary> 定义了数据排序参数的接口。 </summary>
+    public interface ISortParameter
+    {
+        /// <summary> 需要排序的字段。 </summary>
+        /// <value> 设置或获取一个字符串数组，用于表示需要排序的字段。 </value>
+        string[] Fields { get; set; }
+
+        /// <summary> 排序方式。 </summary>
+        /// <value> 设置或获取一个值，用于表示排序方式。 </value>
+        /// <seealso cref="SortMode" />
+        SortMode? Mode { get; set; }
+    }
+}
---
+++ src/Eips.Core/Data/Entity/Queryable/SortMode.cs
@@ -0,0 +1,20 @@
+﻿/* *************************************************************************************************************************************** *\
+ * COPYRIGHT © 2006 - 2022 WANG YUCAI. ALL RIGHTS RESERVED.                                                                                *
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                                      *
+\* *************************************************************************************************************************************** */
+
+namespace Niacomsoft.Eips.Data.Entity.Queryable
+{
+    /// <summary> 定义了数据排序方式枚举类型。 </summary>
+    public enum SortMode
+    {
+        /// <summary> 升序排序。 </summary>
+        Ascending = 1,
+
+        /// <summary> 降序排序。 </summary>
+        Descending = 2,
+
+        /// <summary> 默认的排序方式。等效于 <see cref="Descending" />。 </summary>
+        Default = Descending
+    }
+}
