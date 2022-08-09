f31960ca0a0ae4bd017f5fb9df7cee80ce59b5f3
王 玉才 <netowls-studio@outlook.com>
2022-08-09 11:59:55 +0800

    feat: 新增访问环境变量信息的接口 IEnvironmentVariableAccessor 和 IAsyncEnvironmentVariableAccessor。

---
+++ src/Eips.Core.Abstractions/Core/IAsyncEnvironmentVariableAccessor.cs
@@ -0,0 +1,21 @@
+﻿/* *************************************************************************************************************************************** *\
+ * COPYRIGHT © 2006 - 2022 WANG YUCAI. ALL RIGHTS RESERVED.                                                                                *
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                                      *
+\* *************************************************************************************************************************************** */
+
+using System.Threading.Tasks;
+
+namespace Niacomsoft.Eips
+{
+    /// <summary> 提供了异步访问环境变量信息相关的方法。 </summary>
+    /// <seealso cref="IEnvironmentVariableAccessor" />
+    public interface IAsyncEnvironmentVariableAccessor : IEnvironmentVariableAccessor
+    {
+        /// <summary> (异步的方法) 获取指定名称的环境变量。 </summary>
+        /// <param name="name"> 环境变量名称。 </param>
+        /// <returns> 环境变量信息。 </returns>
+        /// <seealso cref="IEnvironmentVariable" />
+        /// <seealso cref="Task{TResult}" />
+        Task<IEnvironmentVariable> GetEnvironmentVariableAsync(string name);
+    }
+}
---
+++ src/Eips.Core.Abstractions/Core/IEnvironmentVariableAccessor.cs
@@ -0,0 +1,20 @@
+﻿/* *************************************************************************************************************************************** *\
+ * COPYRIGHT © 2006 - 2022 WANG YUCAI. ALL RIGHTS RESERVED.                                                                                *
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                                      *
+\* *************************************************************************************************************************************** */
+
+namespace Niacomsoft.Eips
+{
+    /// <summary> 定义了访问环境变量信息的接口。 </summary>
+    public interface IEnvironmentVariableAccessor
+    {
+        /// <summary> 获取指定名称的环境变量信息。 </summary>
+        /// <param name="name"> 环境变量名称。 </param>
+        /// <returns>
+        /// 环境变量信息。
+        /// <para> 实现了 <see cref="IEnvironmentVariable" /> 类型接口的对象实例。 </para>
+        /// </returns>
+        /// <seealso cref="IEnvironmentVariable" />
+        IEnvironmentVariable GetEnvironmentVariable(string name);
+    }
+}
