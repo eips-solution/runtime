f18f21ebdba3f3c790db6b0c8f22e9af7102de7e
王 玉才 <netowls-studio@outlook.com>
2022-08-10 13:17:51 +0800

    feat: 新增哈希算法工厂接口 IHashAlgorithmsFactory。

---
+++ src/Eips.Core.Abstractions/Security/Cryptography/IHashAlgorithmsFactory.cs
@@ -0,0 +1,27 @@
+﻿/* *************************************************************************************************************************************** *\
+ * COPYRIGHT © 2006 - 2022 WANG YUCAI. ALL RIGHTS RESERVED.                                                                                *
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                                      *
+\* *************************************************************************************************************************************** */
+
+namespace Niacomsoft.Eips.Security.Cryptography
+{
+    /// <summary> 定义了创建哈希算法的工厂接口。 </summary>
+    public interface IHashAlgorithmsFactory
+    {
+        /// <summary>
+        /// 创建哈希算法。
+        /// <para> 实现了 <see cref="IHashAlgorithms" /> 类型接口的对象实例。 </para>
+        /// </summary>
+        /// <param name="alg">
+        /// 哈希算法。
+        /// <para> <see cref="SupportedHashAlgorithms" /> 类型的值。 </para>
+        /// </param>
+        /// <returns>
+        /// 哈希算法对象实例。
+        /// <para> 实现了 <see cref="IHashAlgorithms" /> 类型接口的对象实例。 </para>
+        /// </returns>
+        /// <seealso cref="IHashAlgorithms" />
+        /// <seealso cref="SupportedHashAlgorithms" />
+        IHashAlgorithms Create(SupportedHashAlgorithms alg = SupportedHashAlgorithms.Default);
+    }
+}
