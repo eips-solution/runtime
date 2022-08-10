78710b0e423c5688b699bf267c56f10772dd3576
王 玉才 <netowls-studio@outlook.com>
2022-08-10 16:03:12 +0800

    feat: 新增密码生成接口 IPasswordGenerateUtilities。

---
+++ src/Eips.Core.Abstractions/Security/IPasswordGenerateUtilities.cs
@@ -0,0 +1,20 @@
+﻿/* *************************************************************************************************************************************** *\
+ * COPYRIGHT © 2006 - 2022 WANG YUCAI. ALL RIGHTS RESERVED.                                                                                *
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                                      *
+\* *************************************************************************************************************************************** */
+
+namespace Niacomsoft.Eips.Security
+{
+    /// <summary> 定义了生成密码的接口。 </summary>
+    public interface IPasswordGenerateUtilities
+    {
+        /// <summary> 生成密码的字母表。 </summary>
+        /// <value> 设置或获取一个字符串，用于表示生成密码的字母表。 </value>
+        string Alphabet { get; set; }
+
+        /// <summary> 创建新的密码 </summary>
+        /// <param name="size"> 密码长度。 </param>
+        /// <returns> 新的随机密码。 </returns>
+        string CreateNew(int size = 8);
+    }
+}
