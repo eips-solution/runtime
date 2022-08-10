d5a8bfb18e6d7b78b5fde23c315174a710b59703
王 玉才 <netowls-studio@outlook.com>
2022-08-10 10:50:04 +0800

    feat: 新增哈希算法基类 HashAlgorithms。

---
+++ src/Eips.Core.Abstractions/Security/Cryptography/HashAlgorithms.cs
@@ -0,0 +1,43 @@
+﻿/* *************************************************************************************************************************************** *\
+ * COPYRIGHT © 2006 - 2022 WANG YUCAI. ALL RIGHTS RESERVED.                                                                                *
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                                      *
+\* *************************************************************************************************************************************** */
+
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Niacomsoft.Eips.Security.Cryptography
+{
+    /// <summary> 提供了哈希计算相关的基本方法。 </summary>
+    /// <seealso cref="IHashAlgorithms" />
+    /// <seealso cref="CryptographicAlgorithms" />
+    public abstract class HashAlgorithms : CryptographicAlgorithms, IHashAlgorithms
+    {
+        /// <summary> 用于初始化一个 <see cref="HashAlgorithms" /> 类型的对象实例。 </summary>
+        protected HashAlgorithms() : base()
+        {
+        }
+
+        /// <summary> 哈希算法。 </summary>
+        /// <value>
+        /// 获取 <see cref="HashAlgorithm" /> 类型的对象实例，用于表示哈希算法。
+        /// <para> 派生自 <see cref="HashAlgorithm" /> 类型的对象实例。 </para>
+        /// </value>
+        /// <seealso cref="HashAlgorithm" />
+        protected abstract HashAlgorithm HashAlgorithm { get; }
+
+        /// <summary> 计算哈希。 </summary>
+        /// <param name="input"> 需要计算的字节数组。 </param>
+        /// <returns> 哈希计算后的字节数组。 </returns>
+        public abstract byte[] ComputeHash(byte[] input);
+
+        /// <summary> 计算哈希数据，并写入流 <paramref name="stream" />。 </summary>
+        /// <param name="input"> 字节数组。 </param>
+        /// <param name="stream">
+        /// 目标流。
+        /// <para> 派生自 <see cref="Stream" /> 类型的对象实例。 </para>
+        /// </param>
+        /// <seealso cref="Stream" />
+        public abstract void ComputeHash(byte[] input, Stream stream);
+    }
+}
