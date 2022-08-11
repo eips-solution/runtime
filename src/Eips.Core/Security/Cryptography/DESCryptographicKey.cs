27e3e6064000ec0b77bce70eda885aeddacf9375
王 玉才 <netowls-studio@outlook.com>
2022-08-11 11:36:14 +0800

    feat: 新增 DES 算法密钥生成方法 DESCryptographicKey。

---
+++ src/Eips.Core/Security/Cryptography/DESCryptographicKey.cs
@@ -0,0 +1,32 @@
+﻿/* *************************************************************************************************************************************** *\
+ * COPYRIGHT © 2006 - 2022 WANG YUCAI. ALL RIGHTS RESERVED.                                                                                *
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                                      *
+\* *************************************************************************************************************************************** */
+
+namespace Niacomsoft.Eips.Security.Cryptography
+{
+    /// <summary> 提供了 DES 算法密钥相关的方法。 </summary>
+    /// <seealso cref="ISymmetricCryptographicKey" />
+    /// <seealso cref="SymmetricCryptographicKey" />
+    public class DESCryptographicKey : SymmetricCryptographicKey, ISymmetricCryptographicKey
+    {
+        private readonly string m_internalDesKey;
+
+        /// <summary> 获取密钥字符串。 </summary>
+        /// <returns> 密钥字符串。 </returns>
+        protected override string InternalGetKeyString()
+        {
+            return m_internalDesKey;
+        }
+
+        /// <summary> 用于初始化一个 <see cref="DESCryptographicKey" /> 类型的对象实例。 </summary>
+        public DESCryptographicKey()
+        {
+#if DEBUG
+            m_internalDesKey = "K3LB3T-d";
+#else
+            m_internalDesKey = "sYmc_dkg";
+#endif
+        }
+    }
+}
