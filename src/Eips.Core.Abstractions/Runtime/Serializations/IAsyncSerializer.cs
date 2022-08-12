7d004fcfd3c092024e43820881afd9ae74848cb6
王 玉才 <netowls-studio@outlook.com>
2022-08-12 13:54:18 +0800

    style: 修改了接口 IAsyncSerializer 和 ISerializer 中错误的参数命名。

--- src/Eips.Core.Abstractions/Runtime/Serializations/IAsyncSerializer.cs
+++ src/Eips.Core.Abstractions/Runtime/Serializations/IAsyncSerializer.cs
@@ -13,15 +13,15 @@ namespace Niacomsoft.Eips.Runtime.Serializations
     public interface IAsyncSerializer : ISerializer
     {
         /// <summary>
-        /// (异步的方法) 从流 <paramref name="destionationStream" /> 中反序列化得到
+        /// (异步的方法) 从流 <paramref name="deserializeStream" /> 中反序列化得到
         /// <typeparamref name="T" /> 类型的对象实例。
         /// </summary>
         /// <typeparam name="T"> 引用类型。 </typeparam>
-        /// <param name="destionationStream"> 派生自 <see cref="Stream" /> 类型的对象实例。 </param>
+        /// <param name="deserializeStream"> 派生自 <see cref="Stream" /> 类型的对象实例。 </param>
         /// <returns> <typeparamref name="T" /> 类型的对象实例。 </returns>
         /// <seealso cref="Stream" />
         /// <seealso cref="Task{TResult}" />
-        Task<T> DeserializeAsync<T>(Stream destionationStream) where T : class, new();
+        Task<T> DeserializeAsync<T>(Stream deserializeStream) where T : class, new();
 
         /// <summary>
         /// (异步的方法) 从字节数据 <paramref name="input" /> 反序列化得到
@@ -34,15 +34,15 @@ namespace Niacomsoft.Eips.Runtime.Serializations
         Task<T> DeserializeAsync<T>(byte[] input) where T : class, new();
 
         /// <summary>
-        /// (异步的方法) 序列化 <typeparamref name="T" /> 类型的对象实例，并写入流 <paramref name="destionationStream" />。
+        /// (异步的方法) 序列化 <typeparamref name="T" /> 类型的对象实例，并写入流 <paramref name="serializeStream" />。
         /// </summary>
         /// <typeparam name="T"> 引用类型。 </typeparam>
         /// <param name="obj"> <typeparamref name="T" /> 类型的对象实例。 </param>
-        /// <param name="destionationStream"> 派生自 <see cref="Stream" /> 类型的对象实例。 </param>
+        /// <param name="serializeStream"> 派生自 <see cref="Stream" /> 类型的对象实例。 </param>
         /// <returns> <see cref="Task" /> 类型的对象实例。 </returns>
         /// <seealso cref="Stream" />
         /// <seealso cref="Task" />
-        Task SerializeAsync<T>(T obj, Stream destionationStream) where T : class, new();
+        Task SerializeAsync<T>(T obj, Stream serializeStream) where T : class, new();
 
         /// <summary> (异步的方法) 序列化 <typeparamref name="T" /> 类型的对象实例，并返回字节数据。 </summary>
         /// <typeparam name="T"> 引用类型。 </typeparam>
--- src/Eips.Core.Abstractions/Runtime/Serializations/ISerializer.cs
+++ src/Eips.Core.Abstractions/Runtime/Serializations/ISerializer.cs
@@ -11,14 +11,14 @@ namespace Niacomsoft.Eips.Runtime.Serializations
     public interface ISerializer
     {
         /// <summary>
-        /// 从流 <paramref name="destionationStream" /> 中反序列化得到
+        /// 从流 <paramref name="deserializeStream" /> 中反序列化得到
         /// <typeparamref name="T" /> 类型的对象实例。
         /// </summary>
         /// <typeparam name="T"> 引用类型。 </typeparam>
-        /// <param name="destionationStream"> 派生自 <see cref="Stream" /> 类型的对象实例。 </param>
+        /// <param name="deserializeStream"> 派生自 <see cref="Stream" /> 类型的对象实例。 </param>
         /// <returns> <typeparamref name="T" /> 类型的对象实例。 </returns>
         /// <seealso cref="Stream" />
-        T Deserialize<T>(Stream destionationStream) where T : class, new();
+        T Deserialize<T>(Stream deserializeStream) where T : class, new();
 
         /// <summary>
         /// 从字节数据 <paramref name="input" /> 反序列化得到 <typeparamref name="T" /> 类型的对象实例。
@@ -28,12 +28,12 @@ namespace Niacomsoft.Eips.Runtime.Serializations
         /// <returns> <typeparamref name="T" /> 类型的对象实例。 </returns>
         T Deserialize<T>(byte[] input) where T : class, new();
 
-        /// <summary> 序列化 <typeparamref name="T" /> 类型的对象实例，并写入流 <paramref name="destionationStream" />。 </summary>
+        /// <summary> 序列化 <typeparamref name="T" /> 类型的对象实例，并写入流 <paramref name="serializeStream" />。 </summary>
         /// <typeparam name="T"> 引用类型。 </typeparam>
         /// <param name="obj"> <typeparamref name="T" /> 类型的对象实例。 </param>
-        /// <param name="destionationStream"> 派生自 <see cref="Stream" /> 类型的对象实例。 </param>
+        /// <param name="serializeStream"> 派生自 <see cref="Stream" /> 类型的对象实例。 </param>
         /// <seealso cref="Stream" />
-        void Serialize<T>(T obj, Stream destionationStream) where T : class, new();
+        void Serialize<T>(T obj, Stream serializeStream) where T : class, new();
 
         /// <summary> 序列化 <typeparamref name="T" /> 类型的对象实例，并返回字节数据。 </summary>
         /// <typeparam name="T"> 引用类型。 </typeparam>
