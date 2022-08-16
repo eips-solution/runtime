e492d47baa386a2682c0867eb34e234059e5adab
王 玉才 <netowls-studio@outlook.com>
2022-08-16 12:38:24 +0800

    feat: 新增运行时服务注册扩展服务 RuntimeServicesCollectionExtensions。

---
+++ src/Eips.DependencyInjection/RuntimeServicesCollectionExtensions.cs
@@ -0,0 +1,74 @@
+﻿/* *************************************************************************************************************************************** *\
+ * COPYRIGHT © 2006 - 2022 WANG YUCAI. ALL RIGHTS RESERVED.                                                                                *
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                                      *
+\* *************************************************************************************************************************************** */
+
+using Microsoft.Extensions.DependencyInjection;
+using Niacomsoft.Eips.Extensions;
+using Niacomsoft.Eips.Extensions.Runtime.Serializations;
+using Niacomsoft.Eips.Reflection;
+using Niacomsoft.Eips.Runtime.Serializations;
+using Niacomsoft.Eips.Security;
+using Niacomsoft.Eips.Security.Cryptography;
+
+namespace Niacomsoft.Eips.DependencyInjection
+{
+    /// <summary> 为 <see cref="IServiceCollection" /> 类型提供的扩展方法。 </summary>
+    public static class RuntimeServicesCollectionExtensions
+    {
+        /// <summary> 注册单例模式的运行时服务。 </summary>
+        /// <param name="services">
+        /// 实现了 <see cref="IServiceCollection" /> 类型接口的对象实例。
+        /// </param>
+        private static void AddSingletonRuntimeServicesImpl(IServiceCollection services)
+        {
+            services.AddSingleton<IFluentInterface, FluentInterface>()
+                    .AddSingleton<RandomPasswordGenerator>()
+                    .AddSingleton<GuidGenerator>()
+                    .AddSingleton<NanoidGenerator>();
+        }
+
+        /// <summary> 注册短暂模式的运行时模式。 </summary>
+        /// <param name="services">
+        /// 实现了 <see cref="IServiceCollection" /> 类型接口的对象实例。
+        /// </param>
+        private static void AddTransientRuntimeServicesImpl(IServiceCollection services)
+        {
+            services.AddTransient<MemberAttributeHelper>()
+                    .AddTransient<AssemblyAttributeHelper>()
+                    .AddTransient<EnvironmentVariableAccessor>()
+                    .AddTransient<XmlSerializer>()
+                    .AddTransient<JsonSerializer>()
+                    .AddTransient<MD5AlgorithmsProvider>()
+                    .AddTransient<SHA1AlgorithmsProvider>()
+                    .AddTransient<SHA256AlgorithmsProvider>()
+                    .AddTransient<SHA384AlgorithmsProvider>()
+                    .AddTransient<SHA512AlgorithmsProvider>()
+                    .AddTransient<HashAlgorithmsFactory>()
+                    .AddTransient<ISymmetricCryptographicKey, DESCryptographicKey>()
+                    .AddTransient<DESAlgorithmsProvider>()
+                    .AddTransient<IAESCryptographicKey, AESCryptographicKey>()
+                    .AddTransient<AES128AlgorithmsProvider>()
+                    .AddTransient<AES192AlgorithmsProvider>()
+                    .AddTransient<AES256AlgorithmsProvider>()
+                    .AddTransient<AESAlgorithmsProvider>()
+                    .AddTransient<IHashIdCryptographyKeyProvider, HashIdCryptographyKeyProvider>()
+                    .AddTransient<Int32HashFormatter>()
+                    .AddTransient<Int64HashFormatter>();
+        }
+
+        /// <summary> 注册 EIPS 运行时相关服务。 </summary>
+        /// <param name="services">
+        /// 实现了 <see cref="IServiceCollection" /> 类型接口的对象实例。
+        /// </param>
+        /// <returns> 实现了 <see cref="IServiceCollection" /> 类型接口的对象实例。 </returns>
+        /// <seealso cref="IServiceCollection" />
+        public static IServiceCollection AddEipsRuntimeServices(this IServiceCollection services)
+        {
+            AddSingletonRuntimeServicesImpl(services);
+            AddTransientRuntimeServicesImpl(services);
+
+            return services;
+        }
+    }
+}
