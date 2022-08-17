3083a89aa44fd3d2b9724bd7c47bd8d61a31b981
王 玉才 <netowls-studio@outlook.com>
2022-08-17 13:52:41 +0800

    feat: 新增数据库连接状态变更事件参数 ConnectionStateChangedEventArgs。

---
+++ src/Eips.AdoNet.Abstractions/ConnectionStateChangedEventArgs.cs
@@ -0,0 +1,32 @@
+﻿/* *************************************************************************************************************************************** *\
+ * COPYRIGHT © 2006 - 2022 WANG YUCAI. ALL RIGHTS RESERVED.                                                                                *
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                                      *
+\* *************************************************************************************************************************************** */
+
+using System;
+using System.Data;
+
+namespace Niacomsoft.Eips.Data
+{
+    /// <summary>
+    /// 提供了数据库连接变更事件相关的参数。
+    /// <para> 密闭的，不可以从此类型派生。 </para>
+    /// </summary>
+    /// <seealso cref="EventArgs" />
+    public sealed class ConnectionStateChangedEventArgs : EventArgs
+    {
+        /// <summary>
+        /// 用于初始化一个 <see cref="ConnectionStateChangedEventArgs" /> 类型的对象实例。
+        /// </summary>
+        /// <param name="state">
+        /// 数据库连接状态值。
+        /// <para> <see cref="ConnectionState" /> 类型的值。 </para>
+        /// </param>
+        public ConnectionStateChangedEventArgs(ConnectionState state) => CurrentState = state;
+
+        /// <summary> 当前的连接状态。 </summary>
+        /// <value> 获取一个值，用于表示当前的连接状态。 </value>
+        /// <seealso cref="ConnectionState" />
+        public ConnectionState CurrentState { get; }
+    }
+}
