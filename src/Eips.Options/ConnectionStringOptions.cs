6655951cd8080a6f9f71843a2104cd1ffbcd4af1
王 玉才 <netowls-studio@outlook.com>
2022-08-17 10:43:29 +0800

    feat: 新增连接串配置选项 ConnectionStringOptions。
    <add>- src/Eips.Options/ConnectionStringOptions.cs
    <modify>- src/Eips.Options/EipsOptions.cs

---
+++ src/Eips.Options/ConnectionStringOptions.cs
@@ -0,0 +1,34 @@
+﻿/* *************************************************************************************************************************************** *\
+ * COPYRIGHT © 2006 - 2022 WANG YUCAI. ALL RIGHTS RESERVED.                                                                                *
+ * LICENSED UNDER THE MIT LICENSE. SEE LICENSE FILE IN THE PROJECT ROOT FOR FULL LICENSE INFORMATION.                                      *
+\* *************************************************************************************************************************************** */
+
+namespace Niacomsoft.Eips.Options
+{
+    /// <summary>
+    /// 提供了连接串配置选项相关的方法。
+    /// <para> 密闭的，不可以从此类型派生。 </para>
+    /// </summary>
+    public sealed class ConnectionStringOptions
+    {
+        /// <summary> 连接串。 </summary>
+        /// <value> 设置或获取一个字符串，用于表示连接串。 </value>
+        public string ConnectionString { get; set; }
+
+        /// <summary> 连接串描述信息。 </summary>
+        /// <value> 设置或获取一个字符串，用于表示连接串描述信息。 </value>
+        public string Description { get; set; }
+
+        /// <summary> 连接串名称。 </summary>
+        /// <value> 设置或获取一个字符串，用于表示连接串名称。 </value>
+        public string Name { get; set; }
+
+        /// <summary> 连接程序名称。 </summary>
+        /// <value> 设置或获取一个字符串，用于表示连接程序名称。 </value>
+        public string ProviderName { get; set; }
+
+        /// <summary> 只读的连接串。 </summary>
+        /// <value> 设置或获取一个字符串数组，用于表示只读的连接串。 </value>
+        public string[] ReadonlyConnectionStrings { get; set; }
+    }
+}
--- src/Eips.Options/EipsOptions.cs
+++ src/Eips.Options/EipsOptions.cs
@@ -15,6 +15,15 @@ namespace Niacomsoft.Eips.Options
         /// <summary> 配置选项标识路径。 </summary>
         public const string Key = "eips";
 
+        /// <summary> 连接串配置。 </summary>
+        /// <value> 设置或获取 <see cref="ConnectionStringOptions" /> 类型的对象实例数组，用于表示连接串配置。 </value>
+        /// <seealso cref="ConnectionStringOptions" />
+        public ConnectionStringOptions[] ConnectionStrings { get; set; }
+
+        /// <summary> 默认的连接名称。 </summary>
+        /// <value> 设置或获取一个字符串，用于表示默认的连接名称。 </value>
+        public string DefaultConnection { get; set; }
+
         /// <summary> 企业私有化 SaaS 服务 - 工具配置。 </summary>
         /// <value> 设置或获取 ? 类型的对象实例，用于表示企业私有化 SaaS 服务 - 工具配置。 </value>
         /// <seealso cref="UtilitiesOptions" />
